5b8186c52bf2391f866ef351fb104dee423270bd
Radosław Brzeziński <radekmac@Mac.lan>
2024-10-19 10:42:12 +0200

    fix bugs

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -18,27 +18,27 @@ export const About = () => {
         <div className="about_content_box">
           <h3>Historia</h3>
           <p>
-            Nasza firma powstała z pasji do tworzenia unikalnych i
+            Powstaliśmy z pasji do tworzenia unikalnych i
             spersonalizowanych mebli, które doskonale wpisują się w indywidualne
             potrzeby każdego klienta. Bez względu na to, czy szukasz mebli do
-            swojego domu, biura, restauracji czy hotelu, jesteśmy gotowy, by
+            swojego domu, biura, restauracji czy hotelu, jesteśmy gotowi, by
             przekształcić Twoje wizje w rzeczywistość.
           </p>
         </div>
         <div className="about_content_box">
           <h3>Misja</h3>
           <p>
-            Misją firmy POL-MEBEL jest spełnianie potrzeb i oczekiwania
-            klientów. Firma chce, aby jej meble były nie tylko piękne, ale też
-            funkcjonalne i trwałe. Firma chce, aby jej klienci byli zadowoleni z
+            Naszą misją jest spełnianie potrzeb i oczekiwań
+            klientów. Chcemy aby nasze meble były nie tylko piękne, ale też
+            funkcjonalne i trwałe. Dążymy aby klienci byli zadowoleni z
             zakupionych mebli i polecali je swoim znajomym.
           </p>
         </div>
         <div className="about_content_box">
           <h3>Wizja</h3>
           <p>
-            Wizją firmy jest tworzenie mebli, które będą zmieniać
-            wnętrza na lepsze. Firma chce być liderem w zakresie mebli na
+            Wizją POL-MEBEL jest tworzenie mebli, które będą zmieniać
+            wnętrza na lepsze. Chcemy być liderem w zakresie mebli na
             wymiar, oferując swoim klientom najwyższą jakość, funkcjonalność i
             indywidualność.
           </p>
